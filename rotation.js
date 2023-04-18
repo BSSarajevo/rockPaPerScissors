eab21592d849702b50cc7d32b7507d3d35b2d1f7
Boris <bstanar96@gmail.com>
2023-04-19 00:20:46 +0200

    0

--- rotation.js
+++ rotation.js
@@ -1,7 +1,14 @@
-let anchor=document.querySelector(".svgClass").getSVGDocument().getElementById('MiddleCircle');
-let rekt = anchor.getBoundingClientRect();
+/** initialising constants that i will use trought the game
+ * 
+ */
+// middle circle that is used for determening the mouse angle
+// it will work with inline svg and let anchor=document.getelementbyid('MiddleCircle')
+let anchor=document.getElementsByName('svgClass').document.getElementById('MiddleCircle');
+console.log(anchor.checkVisibility());
 const anchorX = rekt.left + rekt.width / 2;
 const anchorY = rekt.top + rekt.height / 2;
+
+// Rectangle is used for possitioning rock paper scissors icons. Two of them are at the upper corners of the rectangle, while the third is possitioned on the middle of the bottom line 
 anchor = document.querySelector(".svgClass").getSVGDocument().getElementById('Rectangle');
 rekt = anchor.getBoundingClientRect();
 console.log(rekt.cy);
@@ -15,9 +22,9 @@ const lowY=rekt.top-rekt.height/2;
 
 
 
-
+/** used for checking if i got the correct values
 console.log(lowY+"    "+middleY+"  "+highY+" ------ "+(highY-lowY));
-
+*/
 
 document.addEventListener('mousemove', (e) => {
     //Get mouse cordinates
@@ -28,7 +35,7 @@ document.addEventListener('mousemove', (e) => {
     // calculate the angle between the mouse pointer and the centre circle
     const angles = angle(mouseX, mouseY, anchorX, anchorY);
     //Rotating the chooser
-    anchor = document.getElementById('Choser');
+    anchor = document.getElementById('ChooserClean');
     anchor.style.transformOrigin = anchorX + "px " + anchorY + "px";
     anchor.style.transform = "rotate(" + (angles - 150) + "deg)";
 
@@ -37,7 +44,7 @@ document.addEventListener('mousemove', (e) => {
 })
 //Wainting for a click 
 document.addEventListener('click', (e) => {
-    let anchor = document.getElementById('Choser');
+    let anchor = document.getElementById('ChooserClean');
     let rekt = anchor.getBoundingClientRect();
     const choseX = rekt.left + rekt.width / 2;
     const choseY = rekt.top + rekt.height / 2;
@@ -46,10 +53,7 @@ document.addEventListener('click', (e) => {
     const dis = distance(choseX, choseY, paperXY[0], paperXY[1]);
     console.log(dis);
 })
-
-function pointLocation(anchor) {
-
-}
+//function used for determening the angle of the mouse cursor relative to the middle circle
 function angle(cx, cy, ex, ey) {
     const dy = ey - cy;
     const dx = ex - cx;
@@ -57,6 +61,7 @@ function angle(cx, cy, ex, ey) {
     const deg = rad * 180 / Math.PI;
     return deg;
 }
+// will be used for calculating the differance between Rectangle points and the middle point of ChooserClear
 function distance(cx, cy, ex, ey) {
     const dy = ey - cy;
     const dx = ex - cx;
